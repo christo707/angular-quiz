b611b3e34018319d7bf74782e0ab253c0d679506
CHRISTOPHER MICHAEL ROZARIO <christopherrozario7@gmail.com>
2018-10-07 23:24:25 +0530

    Delete firebase.ts

--- src/app/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyDqBuo1Yal9OQNOBGF9HWwDDYcARLKfqK4",
-    authDomain: "angular-quiz-21ae0.firebaseapp.com",
-    databaseURL: "https://angular-quiz-21ae0.firebaseio.com",
-    projectId: "angular-quiz-21ae0",
-    storageBucket: "angular-quiz-21ae0.appspot.com",
-    messagingSenderId: "122864751363"
-  };
